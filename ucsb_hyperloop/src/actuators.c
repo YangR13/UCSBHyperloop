df1f5c530295491cdf55e248aa430bd1c13cdad6
Tristan Seroff <tristan.seroff@gmail.com>
2017-05-23 02:42:10 -0700

    Actuator board code cleanup

--- ucsb_hyperloop/src/actuators.c
+++ ucsb_hyperloop/src/actuators.c
@@ -1,68 +1,52 @@
 #include "actuators.h"
 #include "pwm.h"
+#include "I2CPERIPHS.h"
 
-const uint8_t BOARD_I2C_BUS[NUM_ACTUATOR_BOARDS] = {1, 0, 0, 0}; // TODO: Set me
+const uint8_t BOARD_I2C_BUS[NUM_ACTUATOR_BOARDS] = {1, 0, 0, 0};        // TODO: Set me
 const uint8_t BOARD_I2C_DIP[NUM_ACTUATOR_BOARDS] = {0,  255,  0,  255}; // TODO: Change to realistic values!
 #ifdef ARDUINO
-// Outputs
-#define DIR1 52
-#define PWM1 2
-#define DIR2 53
-#define PWM2 3
-// Inputs
-#define POS1 A0
-#define CUR1 A1
-#define POS2 A2
-#define CUR2 A3
-// TODO: Change these to realistic pin values!
-#define T1 0
-#define T2 0
-#define T3 0
-#define T4 0
-#define FAULT1 0
-#define FAULT2 0
-const uint8_t BOARD_PIN_PORTS[4] = {0, 0, 0, 0, 0, 0};
-const uint8_t BOARD_PINS[12] = {DIR1, DIR2, FAULT1, FAULT2, PWM1, PWM2};
-const uint8_t ARDUINO_ADC_PINS = {POS1, POS2, CUR1, CUR2, T1, T2, T3, T4};
+// Pin values to use for GPIO and PWM signals
+const uint8_t BOARD_PINS[16] =      {52, 53, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t BOARD_PWM_CHANNELS[8] = {2, 3, 0, 0, 0, 0, 0, 0};
+
+// N/A for Arduino but provided for use of LPC-compatible PWM and GPIO functions.
+typedef struct {
+    int dummy_value.
+} LPC_PWM_T;
+const LPC_PWM_T * BOARD_PWM_PORTS[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t BOARD_PIN_PORTS[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 #endif
 #ifdef LPC
+// TODO: Change all of these to realistic pin values!
 
-
-// TODO: Change these to realistic pin values!
-// Board pins, in order, are
-// Board 1 - DIR1, DIR2, FAULT1, FAULT2 | Board 2, etc
+// GPIO pins on the PCB (data values) correspond to, in order:
+//  Board 1 - DIR1, DIR2, FAULT1, FAULT2 | Board 2 (same) | etc.
 const uint8_t BOARD_PIN_PORTS[16] = {3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t BOARD_PINS[16] =      {12, 12, 13, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+// PWM pins on the PCB (data values) correspond to, in order:
+//  Board 1 - PWM1, PWM2 | Board 2 (same), etc.
 const LPC_PWM_T * BOARD_PWM_PORTS[8] = {LPC_PWM1, LPC_PWM1, LPC_PWM1, LPC_PWM1, LPC_PWM1, LPC_PWM1, LPC_PWM1, LPC_PWM1};
 const uint8_t BOARD_PWM_CHANNELS[8] = {5, 6, 4, 3, 2, 1, 1, 1};
-/* Alternate arrangement (in order of ADC channel)
-const uint8_t ADC_PORTS[8] = {LTC2309_CHN_0, LTC2309_CHN_1, LTC2309_CHN_2, LTC2309_CHN_3, LTC2309_CHN_4, LTC2309_CHN_5, LTC2309_CHN_6, LTC2309_CHN_7};
-// ADC ports correspond to: Temp4, Pos2, Temp3, Curr2, Temp2, Pos1, Temp1, Curr1
-*/
-
-const uint8_t ADC_PORTS[8] = {LTC2309_CHN_6, LTC2309_CHN_4, LTC2309_CHN_2, LTC2309_CHN_0, LTC2309_CHN_5, LTC2309_CHN_1, LTC2309_CHN_7, LTC2309_CHN_3};
-// ADC_PORTS correspond to: Temp1, Temp2, Temp3, Temp4, Pos1, Pos2, Curr1, Curr2
 #endif
 
-const uint8_t ADC_Address_Select_actuators[4] = {0x8, 0xA, 0x1A, 0x28};
-
+// This can't be imported from I2CPERIPHS because it's in the .c and not the .h.
+const uint8_t ADC_Address_Select_Actuators[4] = {0x8, 0xA, 0x1A, 0x28};
 
 ACTUATORS* initialize_actuator_board(uint8_t identity) {
   ACTUATORS* board = malloc(sizeof(ACTUATORS));
   board->identity = identity;
   board->bus = BOARD_I2C_BUS[board->identity];
-  board->ADC_device_address = ADC_Address_Select_actuators[(BOARD_I2C_DIP[board->identity] >> 6) & 0b11];
+
+  // TODO: Check if this works right for boards #2 and #3, especially with #0 and #1 present on the same bus.
+  board->ADC_device_address = ADC_Address_Select_Actuators[board->identity];
 
   // Initialize thermistor moving averages (with first read)
-/*  int temp_counter;
+  int temp_counter;
   for (temp_counter = 0; temp_counter < 4; temp_counter++) {
-    board->temperatures[temp_counter] = calculate_temperature(ADC_read_actuators(board->bus, board->ADC_device_address, ADC_PORTS[temp_counter]));
+    board->temperatures[temp_counter] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 6 - (2 * temp_counter)));
   }
-*/
-  board->temperatures[0] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 6));
-  board->temperatures[1] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 4));
-  board->temperatures[2] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 2));
-  board->temperatures[3] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 0));
 
   // Initialize current readings
   int amps_counter;
@@ -76,7 +60,7 @@ ACTUATORS* initialize_actuator_board(uint8_t identity) {
       board->bridge_fault[fault_counter] = 0;
   }
 
-  // Initialize position variables
+  // Initialize position variables (with first read)
   board->position[0] = ADC_read(board->bus, board->ADC_device_address, 1);
   board->position[1] = ADC_read(board->bus, board->ADC_device_address, 5);
 
@@ -95,118 +79,94 @@ ACTUATORS* initialize_actuator_board(uint8_t identity) {
   board->enable[1] = 0;     // Stopped
 
   // Write default values to GPIO/PWM pins
-  GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + 0], BOARD_PINS[(board->identity * 4) + 0], board->direction[0]); // Direction 1
-  GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + 1], BOARD_PINS[(board->identity * 4) + 1], board->direction[1]); // Direction 2
-  PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + 0], BOARD_PWM_CHANNELS[(board->identity * 2) + 0], board->enable[0]); // PWM output 1
-  PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + 1], BOARD_PWM_CHANNELS[(board->identity * 2) + 1], board->enable[1]); // PWM output 2
+  int output_counter = 0;
+  for (output_counter = 0; output_counter < 2; output_counter++){
+      // Direction signal
+      GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + output_counter], BOARD_PINS[(board->identity * 4) + output_counter], board->direction[output_counter]);
+      // PMW enable/speed signal
+      PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + output_counter], BOARD_PWM_CHANNELS[(board->identity * 2) + output_counter], board->enable[output_counter]);
+  }
 
   return board;
 }
 
 uint8_t update_actuator_board(ACTUATORS* board) {
+    // Gathers sensor data from a braking board
+    // Temperature, current, and H-bridge fault signals.
+    // Actuation signals and position feedback are updated in update_actuator_control()
     update_actuator_control(board);
 
     // Record Temperatures
-    board->temperatures[0] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 6));
-    board->temperatures[1] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 4));
-    board->temperatures[2] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 2));
-    board->temperatures[3] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 0));
-
-#if 0
-  int temp_counter;
-  for (temp_counter = 0; temp_counter < NUM_THERMISTORS; temp_counter++) {
-    int new_temperature = calculate_temperature(ADC_read_actuators(board->bus, board->ADC_device_address, ADC_PORTS[temp_counter]));
-    new_temperature = ((1 - THERMISTOR_AVG_WEIGHT) * new_temperature + THERMISTOR_AVG_WEIGHT * board->temperatures[temp_counter]);
-    board->temperatures[temp_counter] = new_temperature;
-    /*
-    if (new_temperature > HEMS_MAX_TEMP || new_temperature < HEMS_MIN_TEMP){
-      board->alarm |= 0b00000001;
+    int temp_counter;
+    for (temp_counter = 0; temp_counter < 4; temp_counter++) {
+      board->temperatures[temp_counter] = calculate_temperature(ADC_read(board->bus, board->ADC_device_address, 6 - (2 * temp_counter)));
+      /*
+        if (new_temperature > HEMS_MAX_TEMP || new_temperature < HEMS_MIN_TEMP){
+          board->alarm |= 0b00000001;
+        }
+      */
     }
-    */
-  }
-#endif
-
 
   // Record Motor Controller Currents
   // H-bridge current sensors output: (50mV offset) + (20 mV / amp)
-  uint16_t raw_reading = ADC_read(board->bus, board->ADC_device_address, 7);
-  uint16_t reading = ((((float)raw_reading * 5.0 / MAX12BITVAL) - 0.050) / 0.020); // Ratio -> volts -> amps
-  board->amps[0] = reading;
+  int current_counter = 0;
+  for (current_counter = 0; current_counter < 2; current_counter++){
+      uint16_t raw_reading = ADC_read(board->bus, board->ADC_device_address, 7 - (4 * current_counter));
+      uint16_t reading = ((((float)raw_reading * 5.0 / MAX12BITVAL) - 0.050) / 0.020); // Ratio -> volts -> amps
+      board->amps[current_counter] = reading;
+      /*
+      if (new_amps > HEMS_MAX_CURRENT){
+          board->alarm |= 0b00000010;
+      }
+      */
+  }
 
-  uint16_t raw_reading_2 = ADC_read(board->bus, board->ADC_device_address, 3);
-  uint16_t reading_2 = ((((float)raw_reading_2 * 5.0 / MAX12BITVAL) - 0.050) / 0.020); // Ratio -> volts -> amps
-  board->amps[0] = reading_2;
-
-//      if (new_amps > HEMS_MAX_CURRENT){
-//          board->alarm |= 0b00000010;
-//      }
-
-  // Read fault flags
-  board->bridge_fault[0] = GPIO_Read(BOARD_PIN_PORTS[(board->identity * 4) + 2], BOARD_PINS[(board->identity * 4) + 2]);
-  board->bridge_fault[1] = GPIO_Read(BOARD_PIN_PORTS[(board->identity * 4) + 3], BOARD_PINS[(board->identity * 4) + 3]);
+  // Read fault signals from H-bridges (GPIO pins)
+  int fault_counter = 0;
+  for (fault_counter = 0; fault_counter < 2; fault_counter++){
+      board->bridge_fault[0] = GPIO_Read(BOARD_PIN_PORTS[(board->identity * 4) + 2], BOARD_PINS[(board->identity * 4) + 2]);
+      board->bridge_fault[1] = GPIO_Read(BOARD_PIN_PORTS[(board->identity * 4) + 3], BOARD_PINS[(board->identity * 4) + 3]);
+  }
 
   //return board->alarm;
   return 0;
 }
 
 void update_actuator_control(ACTUATORS *board){
-    // Read position feedback
-    uint16_t pos_0 = ADC_read(board->bus, board->ADC_device_address, 1);
-    uint16_t pos_1 = ADC_read(board->bus, board->ADC_device_address, 5);
+    // Update data relevant to braking actuator movement
+    //  This function is separated to allow for fast sampling of position feedback
+    // Read position feedback signal from linear potentiometer -> ADC
+    // Write direction signal to GPIO pins, set output PWM frequency
 
+// Position values that are greater than this percentage away from the previous moving average
+//  are considered erroneous and are discarded
 #define MAX_POSITION_DIFF_PCT 0.30
+// Alpha value for moving average window
+#define AVG_ALPHA 0.02
 
-    if (abs(pos_0 - board->position[0]) < (MAX_POSITION_DIFF_PCT * board->position[0])){
-        board->position[0] = (0.02 * pos_0) + (0.98 * board->position[0]);
-    }
-    if (abs(pos_1 - board->position[1]) < (MAX_POSITION_DIFF_PCT * board->position[1])){
-        board->position[1] = (0.02 * pos_1) + (0.98 * board->position[1]);
+    // Read and process position feedback
+    //  Calculate a moving average using only those values not considered erroneous
+    int pos_counter = 0;
+    for (pos_counter = 0; pos_counter < 2; pos_counter++){
+        uint16_t pos = ADC_read(board->bus, board->ADC_device_address, (4 * pos_counter) + 1);
+        if (abs(pos - board->position[pos_counter]) < (MAX_POSITION_DIFF_PCT * board->position[pos_counter])){
+            board->position[pos_counter] = (AVG_ALPHA * pos) + ((1 - AVG_ALPHA) * board->position[pos_counter]);
+        }
     }
 
     // Update direction and PWM
-    GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + 0], BOARD_PINS[(board->identity * 4) + 0], board->direction[0]); // Direction 1
-    GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + 1], BOARD_PINS[(board->identity * 4) + 1], board->direction[1]); // Direction 2
-    PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + 0], BOARD_PWM_CHANNELS[(board->identity * 2) + 0], board->enable[0]); // PWM output 1
-    PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + 1], BOARD_PWM_CHANNELS[(board->identity * 2) + 1], board->enable[1]); // PWM output 2
-
+    int output_counter = 0;
+    for (output_counter = 0; output_counter < 2; output_counter++){
+        // Direction signal
+        GPIO_Write(BOARD_PIN_PORTS[(board->identity * 4) + output_counter], BOARD_PINS[(board->identity * 4) + output_counter], board->direction[output_counter]);
+        // PMW enable/speed signal
+        PWM_Write(BOARD_PWM_PORTS[(board->identity * 2) + output_counter], BOARD_PWM_CHANNELS[(board->identity * 2) + output_counter], board->enable[output_counter]);
+    }
 }
-
-/*
-void GPIO_Setup(uint8_t port, uint8_t pin, uint8_t dir) {
-#ifdef ARDUINO
-  if (dir == IN) pinMode(AUX_PIN, INPUT);
-  else if (dir == OUT) pinMode(AUX_PIN, OUTPUT);
-#endif //ARDUINO
-#ifdef LPC
-  if (dir == IN) GPIO_Input_Init(port, pin);
-  else if (dir == OUT) GPIO_Output_Init(port, pin);
-#endif //LPC
-}
-
-void GPIO_Write(uint8_t port, uint8_t pin, uint8_t setting) {
-#ifdef ARDUINO
-  digitalWrite(AUX_PIN, setting);
-#endif //ARDUINO
-#ifdef LPC
-  Chip_GPIO_SetPinState(LPC_GPIO, port, pin, setting);
-#endif //LPC
-}
-
-uint8_t GPIO_Read(uint8_t port, uint8_t pin) {
-#ifdef ARDUINO
-  return digitalRead(AUX_PIN);
-#endif //ARDUINO
-#ifdef LPC
-  return Chip_GPIO_GetPinState(LPC_GPIO, port, pin);
-#endif //LPC
-}
-*/
-
 
 void PWM_Setup(const void * pwm, uint8_t pin){
 #ifdef ARDUINO
-    // Do arduino things.
-    // set as output.
+    pinMode(pin, OUTPUT);
 #endif
 #ifdef LPC
     Init_Channel((LPC_PWM_T *) pwm, pin);
@@ -221,153 +181,3 @@ void PWM_Write(const void * pwm, uint8_t pin, float duty) {
     Set_Channel_PWM((LPC_PWM_T *) pwm, pin, duty);
 #endif // LPC
 }
-
-/*
-uint16_t ADC_read_actuators(uint8_t bus, uint8_t ADC_address, uint8_t ADC_channel) {
-  uint8_t input_buffer[2];
-
-#ifdef ARDUINO
-  Wire.beginTransmission(ADC_address);
-  Wire.write(ADC_CHANNEL_SELECT[ADC_channel]);
-  Wire.endTransmission(true);
-  Wire.requestFrom(ADC_address, 2, true);
-  input_buffer[0] = Wire.read();  //D11 D10 D9 D8 D7 D6 D5 D4
-  input_buffer[1] = Wire.read();  //D3 D2 D1 D0 X X X X
-#endif //ARDUINO
-#ifdef LPC
-  uint8_t channel = (ADC_channel | ADC_CONFIG);
-  Chip_I2C_MasterSend(bus, ADC_address, &channel, 1);
-  Chip_I2C_MasterRead(bus, ADC_address, input_buffer, 2);
-#endif //LPC
-
-  uint16_t ADC_value = (input_buffer[0] << 4) | (input_buffer[1] >> 4);
-  return ADC_value;
-}
-*/
-#if 0
-
-#define PWM_CYCLE 253
-#define ALPHA 0.5
-#define WINDOW 5
-
-//initialize variables
-int target = 0;
-int is_adjusting_1 = 0;
-int location_1 = 0;
-int feedback_1 = 0;
-int is_adjusting_2 = 0;
-int location_2 = 0;
-int feedback_2 = 0;
-
-void setup() {
-
-  //enable output pins
-  pinMode(DIR1, OUTPUT);
-  pinMode(DIR2, OUTPUT);
-  digitalWrite(DIR1, LOW);
-  digitalWrite(DIR2, LOW);
-  analogWrite(PWM1, 0);
-  analogWrite(PWM2, 0);
-
-  //begin serial communication
-  Serial.begin(9600);
-
-}
-
-void loop() {
-
-  if(Serial.available() > 0) { //input available
-
-    //read input, set as new target
-    target = Serial.parseInt();
-    if(target < 17) target = 17;
-    if(target > 555) target = 555;
-    Serial.println();
-    Serial.println(target);
-    Serial.println();
-    is_adjusting_1 = 1;
-    is_adjusting_2 = 1;
-
-  }
-
-  if(is_adjusting_1 == 1) { //evaluate behavior
-
-    //read feedback
-    feedback_1 = analogRead(POS1);
-    //Serial.println(feedback);
-
-    //update location, rolling average filter
-    location_1 = (1-ALPHA)*location_1 + ALPHA*feedback_1;
-    Serial.print(feedback_1);
-
-    //determine direction of motion
-    if(((target - WINDOW) < location_1) && (location_1 < (target + WINDOW))) { //at correct location
-
-      //disable output
-      analogWrite(PWM1,0);
-      is_adjusting_1 = 0;
-
-    }
-
-    else {
-      //Serial.println(location);
-      if(location_1 <= (target - WINDOW)) { //has to advance
-
-        //advance
-        digitalWrite(DIR1, HIGH);
-        analogWrite(PWM1, PWM_CYCLE);
-
-      }
-
-      else { //has to retract
-
-        //retract
-        digitalWrite(DIR1, LOW);
-        analogWrite(PWM1, PWM_CYCLE);
-
-      }
-    }
-  }
-
-    if(is_adjusting_2 == 1) { //evaluate behavior
-
-    //read feedback
-    feedback_2 = analogRead(POS2);
-    //Serial.println(feedback);
-
-    //update location, rolling average filter
-    location_2 = (1-ALPHA)*location_2 + ALPHA*feedback_2;
-    Serial.print('\t');
-    Serial.print(location_2);
-
-    //determine direction of motion
-    if(((target - WINDOW) < location_2) && (location_2 < (target + WINDOW))) { //at correct location
-
-      //disable output
-      analogWrite(PWM2,0);
-      is_adjusting_2 = 0;
-
-    }
-
-    else {
-      if(location_2 <= (target - WINDOW)) { //has to advance
-
-        //advance
-        digitalWrite(DIR2, HIGH);
-        analogWrite(PWM2, PWM_CYCLE);
-
-      }
-
-      else { //has to retract
-
-        //retract
-        digitalWrite(DIR2, LOW);
-        analogWrite(PWM2, PWM_CYCLE);
-
-      }
-    }
-  }
-  Serial.println();
-}
-#endif
-
--- ucsb_hyperloop/src/actuators.h
+++ ucsb_hyperloop/src/actuators.h
@@ -22,43 +22,20 @@
 #include "timer.h"
 #endif //LPC
 
-
 // Number of Boards
 #define NUM_ACTUATORS 2 // Per board
 #define NUM_THERMISTORS 4 // Per board
 #define NUM_ACTUATOR_BOARDS 4
-
-// Thermistor Data
-#define REFERENCE_RESISTANCE 5100 //[ohms]
-#define THERMISTOR_BETA 3380
-#define THERMISTOR_OFFSET -2.126
-
-// Ammeter Data
-#define AMMETER_10A_SENSITIVITY 264
-#define AMMETER_50A_SENSITIVITY 40
-#define AMMETER_150A_SENSITIVITY 8.8        //[mV/A] for the 150amp version of the sensor
 
 //Safety:
 #define ACTUATOR_MAX_TEMP 60      // Too hot
 #define ACTUATOR_MIN_TEMP 5       // More to detect disconnects than for "too cold"
 #define ACTUATOR_MAX_CURRENT 50
 
-//Averaging:
-#define TACHOMETER_AVG_WEIGHT 0.2 //Out of 1 (value = (old_value * AVG_WEIGHT + (1 - AVG_WEIGHT) * new_value) Set to 0 if you don't want exponential averaging.
-#define THERMISTOR_AVG_WEIGHT 0.4 //Out of 1 (value = (old_value * AVG_WEIGHT + (1 - AVG_WEIGHT) * new_value)
-
 #define MAX12BITVAL 4095.0
 
 #define IN 0
 #define OUT 1
-void GPIO_Setup(uint8_t port, uint8_t pin, uint8_t dir);
-void GPIO_Write(uint8_t port, uint8_t pin, uint8_t setting);
-uint8_t GPIO_Read(uint8_t port, uint8_t pin);
-
-
-// These are const to make the compiler happy.
-void PWM_Setup(const void * pwm, uint8_t pin);
-void PWM_Write(const void * pwm, uint8_t pin, float duty);
 
 typedef struct {
   uint8_t identity;
@@ -86,43 +63,11 @@ uint8_t update_actuator_board(ACTUATORS* board);
 void update_actuator_control(ACTUATORS *board);
 int calculate_temperature(uint16_t therm_adc_val);
 
-/*HEMS I2C Parameters
-  Device Addressing (7-bit addressing):
-  ADC LTC2309: 0 ...                                  -0??10??-   //Tri-state inputs A0 and A1, however we'll not use float (don't need that many addresses)
-  I2C_DIP: 0b?????XXX   //X = don't cares; can be anything. They're not connected.
-*/
+// The PWM channels are defined in a const array so the parameters are const accordingly.
+void PWM_Setup(const void * pwm, uint8_t pin);
+void PWM_Write(const void * pwm, uint8_t pin, float duty);
 
-/*ADC LTC2309
-   Max I2C Clock Frequency: 400kHz
-  I2C Protocol:
-    1. Master Write: Device Address (with Write Bit)
-    2. Master Write: DIN (Input Data Word)
-    3. Master Write: Device Address (with Read Bit)
-    4. Master Read: 2 Bytes
-  Input Data Word (6-bit)
-  S/D | O/S | S1 | S0 | UNI | SLP | X | X
-  S/D = Single-Ended/_Differential (We'll always use Single-Ended here, so = 1)
-  O/S = Odd/_Sign (Used to select which channel)
-  S1 = Channel Select Bit 1 (Also used to select which channel)
-  S2 = Channel Select Bit 0 (Also used to select which channel)
-  UNI = Unipolar/_Bipolar (We'll always use Unipolar, so = 1)
-  SLP = Sleep Mode (We won't put this into sleep mode, so = 0)
-*/
-#define ADC_CONFIG 0x88     //0b1???10XX
-
-// Single-Ended Channel Configuration
-#define LTC2309_CHN_0   0x80
-#define LTC2309_CHN_1   0xC0
-#define LTC2309_CHN_2   0x90
-#define LTC2309_CHN_3   0xD0
-#define LTC2309_CHN_4   0xA0
-#define LTC2309_CHN_5   0xE0
-#define LTC2309_CHN_6   0xB0
-#define LTC2309_CHN_7   0xF0
-
-//ADC Associated Functions:
-//uint16_t ADC_read_actuators(uint8_t i2c_bus, uint8_t ADC_address, uint8_t ADC_channel);
-uint16_t ADC_read(uint8_t i2c_bus, uint8_t ADC_address, uint8_t ADC_channel);
+// See I2CPERIPHS.h for details on I2C ADC addressing
 
 
 #endif //ACTUATORS_H
