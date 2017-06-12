50e2138ce45c42a464261b58238969068be8f8c1
Tristan Seroff <tristan.seroff@gmail.com>
2017-06-12 13:08:30 -0700

    Add debug data printouts for 18V5 and power distribution BMS boards

--- ucsb_hyperloop/src/sensor_data.c
+++ ucsb_hyperloop/src/sensor_data.c
@@ -204,6 +204,25 @@ void printSensorData(){
         DEBUGOUT("\n");
     }
 
+    if (BMS_18V5_ACTIVE){
+        DEBUGOUT("18V5 BMS sensors: \n");
+        int j = 0;
+        for (j = 0; j < 4; j++){
+            DEBUGOUT("Batt %d: %f v \t cell voltages %f \t %f \t %f \t %f \t %f \t temperatures %d \t %d \n", j, bms_18v5->battery_voltage[j], bms_18v5->cell_voltages[j][0], bms_18v5->cell_voltages[j][1], bms_18v5->cell_voltages[j][2], bms_18v5->cell_voltages[j][3], bms_18v5->cell_voltages[j][4], bms_18v5->temperatures[j][0], bms_18v5->temperatures[j][1]);
+            DEBUGOUT("Current sensor %d: %f \n", j, bms_18v5->amps);
+        }
+        DEBUGOUT("\n");
+    }
+
+    if (PWR_DST_BMS_ACTIVE){
+        DEBUGOUT("Power Distribution BMS sensors: \n");
+        int j = 0;
+        for (j = 0; j < 2; j++){
+            DEBUGOUT("Batt %d: %f v \t cell voltages %f \t %f \t %f \t %f \t %f \t temperatures %d \t %d \n", j, pwr_dst_bms->battery_voltage[j], pwr_dst_bms->cell_voltages[j][0], pwr_dst_bms->cell_voltages[j][1], pwr_dst_bms->cell_voltages[j][2], pwr_dst_bms->cell_voltages[j][3], pwr_dst_bms->cell_voltages[j][4], pwr_dst_bms->temperatures[j][0], pwr_dst_bms->temperatures[j][1]);
+        }
+        DEBUGOUT("\n");
+    }
+
     if (CONTACT_SENSOR_ACTIVE){
         DEBUGOUT("contact_sensor_pushed: %d\n", sensorData.contact_sensor_pushed);
     }
